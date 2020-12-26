0d77b33eaa79eb1246bfc9687df60c0388741c77
David Bruant <davidbruant@protonmail.com>
2020-12-26 23:03:44 +0100

    Conversion à des espaces plutôt que des tabs

--- direct/components/TripRequestEntry.js
+++ direct/components/TripRequestEntry.js
@@ -6,17 +6,17 @@ import escapeRegexp from 'escape-string-regexp'
 const html = htm.bind(React.createElement)
 
 const cityInputElement = styled.input`
-	margin: 0 0.6rem 0 0.6rem;
-	border: 3px solid steelblue;
-	border-radius: 0.3rem;
-	padding: 0.2rem 0.3rem;
-	font-size: 110%;
+    margin: 0 0.6rem 0 0.6rem;
+    border: 3px solid steelblue;
+    border-radius: 0.3rem;
+    padding: 0.2rem 0.3rem;
+    font-size: 110%;
 `
 
 const datalistId = "valid-place-names"
 
 function makeInputPatternFromList(list){
-	return [...list].map(s => escapeRegexp(s)).join('|')
+    return [...list].map(s => escapeRegexp(s)).join('|')
 }
 
 // styledLabel is defined outside of CityInput because if it's defined inside,
@@ -26,74 +26,74 @@ function makeInputPatternFromList(list){
 const styledLabel = styled.label` display: block; `
 
 const CityInput = ({ label, validPlaceNames, value, setValue }) => {
-	const pattern = makeInputPatternFromList(validPlaceNames)
-	const validationMessage = `Vous devez saisir un de ces lieux : ${validPlaceNames.join(', ')}`
+    const pattern = makeInputPatternFromList(validPlaceNames)
+    const validationMessage = `Vous devez saisir un de ces lieux : ${validPlaceNames.join(', ')}`
 
-	return html`
-		<${styledLabel}>
-			<${styled.strong`
-				display: inline-block;
-				width: 4.5rem;
-			`}>${label}</strong>
-			<${cityInputElement}
-				type="text"
-				list=${datalistId}
-				pattern=${pattern}
-				value=${value}
-				onChange=${e => {
-					e.target.setCustomValidity('');
-					const value = e.target.value
-					setValue(value)
-				}}
-				onInvalid=${e => {
-					e.target.setCustomValidity(validationMessage);
-				}}
-			/>
-		</label>
-	`
+    return html`
+        <${styledLabel}>
+            <${styled.strong`
+                display: inline-block;
+                width: 4.5rem;
+            `}>${label}</strong>
+            <${cityInputElement}
+                type="text"
+                list=${datalistId}
+                pattern=${pattern}
+                value=${value}
+                onChange=${e => {
+                    e.target.setCustomValidity('');
+                    const value = e.target.value
+                    setValue(value)
+                }}
+                onInvalid=${e => {
+                    e.target.setCustomValidity(validationMessage);
+                }}
+            />
+        </label>
+    `
 }
 
 export default function TripRequestEntry({
-	tripRequest,
-	validPlaceNames,
-	onTripRequestChange
+    tripRequest,
+    validPlaceNames,
+    onTripRequestChange
 }) {
-	const [origin, setOrigin] = useState( tripRequest.origin )
-	const [destination, setDestination] = useState( tripRequest.destination )
+    const [origin, setOrigin] = useState( tripRequest.origin )
+    const [destination, setDestination] = useState( tripRequest.destination )
 
-	return html`
-		<${styled.h2`
-			text-align: center;
-			margin: 0 0 1.5rem;
-		`} key="h2">Où allez-vous ?</h2>
-		<form key="form" className="trip-request-entry" onSubmit=${e => {
-			e.preventDefault()
-			onTripRequestChange({ origin, destination })
-		}}>
-			<datalist id=${datalistId}>
-				${validPlaceNames.map(validPlaceName => {
-					return html`
-						<option key=${validPlaceName} value=${validPlaceName} />
-					`
-				})}
-			</datalist>
-			<section className="geography">
-				<${CityInput} 
-					key="départ" 
-					label="Départ"
-					validPlaceNames=${validPlaceNames}
-					value=${origin}
-					setValue=${setOrigin}
-				/>
-				<${CityInput}
-					key="arrivée"
-					label="Arrivée"
-					validPlaceNames=${validPlaceNames}
-					value=${destination}
-					setValue=${setDestination}
-				/>
-			</section>
-			<button type="submit">Rechercher</button>
-		</form>
-	`
+    return html`
+        <${styled.h2`
+            text-align: center;
+            margin: 0 0 1.5rem;
+        `} key="h2">Où allez-vous ?</h2>
+        <form key="form" className="trip-request-entry" onSubmit=${e => {
+            e.preventDefault()
+            onTripRequestChange({ origin, destination })
+        }}>
+            <datalist id=${datalistId}>
+                ${validPlaceNames.map(validPlaceName => {
+                    return html`
+                        <option key=${validPlaceName} value=${validPlaceName} />
+                    `
+                })}
+            </datalist>
+            <section className="geography">
+                <${CityInput} 
+                    key="départ" 
+                    label="Départ"
+                    validPlaceNames=${validPlaceNames}
+                    value=${origin}
+                    setValue=${setOrigin}
+                />
+                <${CityInput}
+                    key="arrivée"
+                    label="Arrivée"
+                    validPlaceNames=${validPlaceNames}
+                    value=${destination}
+                    setValue=${setDestination}
+                />
+            </section>
+            <button type="submit">Rechercher</button>
+        </form>
+    `
 }
